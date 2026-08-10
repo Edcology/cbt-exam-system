30275052b33dc78ade977fefabcfbc5fc4b25945
Awofala Gbolahan <edawofal@gmail.com>
2026-08-10 14:50:32 +0100

    Configure Render persistent disk at /data/cbt_database.sqlite

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 const bcrypt = require('bcryptjs');
 
-const dbPath = path.resolve(__dirname, 'cbt_database.sqlite');
+const dbPath = process.env.DB_PATH || path.resolve(__dirname, 'cbt_database.sqlite');
 const db = new sqlite3.Database(dbPath);
 
 // Promisify database operations
